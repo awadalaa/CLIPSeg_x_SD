dbe37f1bda036a5227b55653dd527ef671200a7d
Alaa Awad <alaa.awad.mail@gmail.com>
2022-12-10 23:16:10 -0500

    .

--- app.py
+++ app.py
@@ -9,17 +9,8 @@ import cv2
 
 clip_seg_processor = CLIPSegProcessor.from_pretrained("CIDAS/clipseg-rd64-refined")
 clip_seg_model = CLIPSegForImageSegmentation.from_pretrained("CIDAS/clipseg-rd64-refined")
-auth_token = os.environ.get("HF_TOKEN") or True
-sd_inpainting_model = StableDiffusionInpaintPipeline.from_pretrained(
-  "stable-diffusion-2-inpainting",
-  revision="fp16",
-  torch_dtype=torch.float32,
-  use_auth_token=auth_token
-)
-sd_inpainting_model.scheduler = DPMSolverMultistepScheduler.from_config(sd_inpainting_model.scheduler.config)
 
-
-def process_image(image, prompt1, prompt2):
+def process_image(image, prompt):
   inputs = clip_seg_processor(text=prompt, images=image, padding="max_length", return_tensors="pt")
 
   # predict
@@ -31,13 +22,7 @@ def process_image(image, prompt1, prompt2):
   plt.imsave(filename_mask, torch.sigmoid(preds))
   mask_image = Image.open(filename_mask).convert("RGB")
 
-  with torch.no_grad():
-    output_image = sd_inpainting_model(prompt=prompt2, image=image, mask_image=mask_image).images[0]
-  filename_sd = f"sd.png"
-  output_image.save(filename_sd)
-  sd_image = Image.open(filename_sd).convert("RGB")
-
-  return [mask_image, sd_image]
+  return mask_image
 
 
 
@@ -51,10 +36,8 @@ interface = gr.Interface(fn=process_image,
                      inputs=[
                         gr.Image(type="pil"),
                         gr.Textbox(label="What to identify"),
-                        gr.Textbox(label="What to replace it with"),
                       ],
                      outputs=[
-                        gr.Image(type="pil"),
                         gr.Image(type="pil"),
                      ],
                      title=title,
